4fbcccdc1e53e3b28e579aa9b1f6ff7d38d7e0b2
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-11 15:32:42 -0700

    added first location

--- js/app.js
+++ js/app.js
@@ -19,32 +19,79 @@
 // Alki	2	16	4.6
 
 'use strict';
+
+var salesData = document.getElementById('saledata');
 //my first object 'firstandpike' with the property of mincust, maxcust, and avgCookiesale and each property has a value 
 var firstAndPike = {
     locationName: '1st And Pike',
     minCust: 23,//the min. number of customers per hr.
     maxCust: 65,//the max. number of customers per hr.
     avgCookieSale: 6.3,// the average number of cookies purchased per customer
+    storeCookies: [],
     //need the average customer per hr (23+65)/2
     //how many cookies sold per hr = (the average customer per hour * the average cookies purchased per customer)
     
     makeRandomFirstAndPike: function (){
         var minimum = Math.floor(this.minCust);
         var maximum = Math.floor(this.maxCust);
-        var averageCookieSale = Math.random(this.avgCookieSale);
-       // var avgCookie = Math.random() * (maximum - minimum +1) + minimum;
-        var average = (minimum + maximum) / 2;
-        var cookiesPerHour = Math.floor(average * averageCookieSale);
-             console.log(cookiesPerHour);
+       
+       var numberCustomer = Math.random() * (maximum - minimum +1) + minimum;
+       var cookiesPerHour = Math.round(numberCustomer * this.avgCookieSale);
+       
+       return cookiesPerHour;         
     }
-
 };
-   
-//console.log(firstAndPike.cookiesPerDay);
+
+   firstAndPike.eachHour = function(){
+       for (var i = 0; i < 15; i++){
+        this.storeCookies.push(this.makeRandomFirstAndPike())
+        
+       };
+   };
+   console.log(firstAndPike.storeCookies);
+
+   firstAndPike.mySaleData = function(){
+        this.eachHour();
+
+        var openHour = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm']
 
 
+        var liEl = document.createElement('li');
+        var h2 = document.createElement('h2');
+       
+        h2.textContent= this.locationName;
+        liEl.appendChild(h2);
+        var ulEl = document.createElement('ul');
+
+        
+
+        var total = 0;
+        
+        for (var i=0; i < this.storeCookies.length; i++){
+            var liElHour = document.createElement('li');
+                liElHour.textContent= openHour[i] + ': ' + this.storeCookies[i] + ' cookies';
+            
+                ulEl.appendChild(liElHour);
+                total = total + this.storeCookies[i]
+                
+        }
+        
+        var totalLi = document.createElement('li');
+            //totalLi.textContent= total;
+            totalLi.textContent= 'Total ' + ': ' + total;
+        ulEl.appendChild(totalLi);
+        liEl.appendChild(ulEl);
+        salesData.appendChild(liEl);
+        
+       
+    }
+    firstAndPike.mySaleData();
 
 
+  
+
+
+///////////////////////////
 var seaTacAirport = {
     minCust: 3,
     maxCust: '24',
