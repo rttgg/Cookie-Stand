032be25c585c9fba8c049de8a51312b1ec13293f
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-14 06:49:39 -0700

    adding img and styling

---
+++ js/test.js
@@ -0,0 +1,92 @@
+// 'use strict';
+
+
+// var tableEl = document.getElementById('store-location');
+
+// var trEl = document.createElement('tr');
+// var tdEl = document.createElement('td');
+// tdEl.textContent = '6:00am';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '7:00am)';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '8:00am';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '9:00am';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '10:00am';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '11:00am';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '12:00am';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '1:00pm';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '2:00pm';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '3:00pm';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '4:00pm';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '5:00pm';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '6:00pm';
+// trEl.appendChild(tdEl);
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = '7:00pm';
+// trEl.appendChild(tdEl);
+
+
+// tableEl.appendChild(trEl);
+
+
+
+
+// trEl = document.getElementById('first-and-pike');
+
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+// tdEl = document.createElement('td');
+// tdEl.textContent = 'first-and-pike';
+// trEl.appendChild(tdEl);
+
