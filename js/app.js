fc9ba76d3e6de9c3e2fabbd221d09e19b7a1907c
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-19 21:33:58 -0700

    fix my total row

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 //declare global variables
 var storeTable = document.getElementById('store-table');
 var storeArr = [];
+var totals = [];
+var trEl = document.createElement('tr');
+var tdEl = document.createElement('td');
 var hours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm', '7:00pm','8:00pm'];
 
 //define object class, This is my store constructor. I will be using this to build my stores
@@ -13,6 +16,7 @@ var SalmonCookieShop = function(location,minCust,maxCust,avgCookies, cookiesSold
   this.cookiesSoldPerHour = cookiesSoldPerHour;
 };
 
+
 storeArr.push(new SalmonCookieShop('1st and Pike',23,65,6.3));
 storeArr.push(new SalmonCookieShop('SeaTac Airport',3,24,1.2));
 storeArr.push(new SalmonCookieShop('Seattle Center',11,38,3.7));
@@ -23,7 +27,6 @@ storeArr.push(new SalmonCookieShop('Alki',2,16,4.6));
 SalmonCookieShop.prototype.customersPerHour = function(){
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
 };
-
 //calculate total daily sales
 SalmonCookieShop.prototype.calculatecookiesSoldPerHour= function(){
   var customers = this.customersPerHour();
@@ -40,10 +43,13 @@ SalmonCookieShop.prototype.calculateTotalCookiesPerHour = function(){
   }
   this.total = total;
 };
+
+
 function makeHeader(){
   var theadEl = document.createElement('thead');
-  var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
+  trEl = document.createElement('tr');
+  thEl = document.createElement('th');
   thEl.textContent = '';
   trEl.appendChild(thEl);
   for(var i = 0; i < hours.length; i++){
@@ -64,8 +70,8 @@ SalmonCookieShop.prototype.addRow = function(){
     this.calculateTotalCookiesPerHour();
   }
 
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
+  trEl = document.createElement('tr');
+  tdEl = document.createElement('td');
   tdEl.textContent = this.location;
   trEl.appendChild(tdEl);
 
@@ -74,7 +80,6 @@ SalmonCookieShop.prototype.addRow = function(){
     tdEl.textContent = this.cookiesSoldPerHour[j];
     trEl.appendChild(tdEl);
   }
-
   tdEl = document.createElement('td');
   tdEl.textContent = this.total;
   trEl.appendChild(tdEl);
@@ -82,30 +87,48 @@ SalmonCookieShop.prototype.addRow = function(){
   storeTable.appendChild(trEl);
 };
 
+
 function sumOfDailyLocationTotals(){
   var allTotals = 0;
   for(var i = 0; i < storeArr.length; i++){
     allTotals += storeArr[i].total;
   }
+  console.log(allTotals);
   return allTotals;
 }
 
-//Add Totals row
+// this function to add additional store and be able to added up with the rest of the store
+function clearTotals(){
+  for(var i = 0; i < hours.length; i++){
+    totals[i] = 0;
+  }
+}
 
+function calcTotals(){
+  clearTotals();
+  for(var i = 0; i < hours.length; i++){
+    for(var j = 0; j < storeArr.length; j++){
+      totals[i] += storeArr[j].cookiesSoldPerHour[i];
+    }
+  }
+}
+
+
+
+//Add Totals row
 function makeTotalsRow(){
   var tfootEl = document.createElement('tfoot');
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
+  trEl = document.createElement('tr');
+  trEl.id = 'totalRow';
+  tdEl = document.createElement('td');
   tdEl.textContent = 'Totals';
   trEl.appendChild(tdEl);
 
-  for(var i = 0; i < hours.length; i++){
+  calcTotals();
+  for(var i = 0; i < totals.length; i++){
     tdEl = document.createElement('td');
-    var totalperHour = 0;
-    for (var j = 0; j < storeArr.length; j++){
-      totalperHour+= storeArr[j].cookiesSoldPerHour[i];
-    }
-    tdEl.textContent = totalperHour;
+    console.log(totals[i]);
+    tdEl.textContent = totals[i];
     trEl.appendChild(tdEl);
   }
 
@@ -124,22 +147,26 @@ function makePage(){
   }
   makeTotalsRow();
 }
+
 makePage();
 
 //MAKING FORM
-
 var form = document.getElementById('add-store-form');
 var handleAddStoreForm = function(e){
   e.preventDefault();
-
   var formLocation = e.target.location.value;
   var formMinCust = e.target.minCust.value;
   var formMaxCust = e.target.maxCust.value;
   var formAvgCookies = e.target.avgCookies.value;
-
   var newSalmonCookiesShop = new SalmonCookieShop(formLocation, formMinCust, formMaxCust, formAvgCookies);
-  newSalmonCookiesShop.addRow();
   storeArr.push(newSalmonCookiesShop);
-
+  newSalmonCookiesShop.addRow();
+  var totalRow = document.getElementById('totalRow');
+  if(totalRow){
+    totalRow.remove();
+    makeTotalsRow();
+  }
 };
+
+
 form.addEventListener('submit', handleAddStoreForm);
